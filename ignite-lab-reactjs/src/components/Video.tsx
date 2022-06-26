d0d8ac5e3a44c0daf1feb10d48b660048e93fb38
flaviaprates <flaviadeaprates@gmail.com>
2022-06-26 17:10:12 -0300

    Ignite finalizado

--- ignite-lab-reactjs/src/components/Video.tsx
+++ ignite-lab-reactjs/src/components/Video.tsx
@@ -1,49 +1,22 @@
 import { DefaultUi, Player, Youtube } from "@vime/react";
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from "phosphor-react";
-import { gql, useQuery } from "@apollo/client";
 
 import '@vime/core/themes/default.css';
+import { useGetLessonBySlugQuery } from "../graphql/generated";
 
-const GET_LESSON_BY_SLUG_QUERY = gql`
-query GetLessonBySlug ($slug: String) {
-  lesson(where: {slug: $slug}) {
-    title
-    videoId
-    description
-    teacher {
-      name
-      bio
-      avatarURL
-    }
-  }
-}
-`
-
-interface GetLessonBySlugResponse {
-    lesson: {
-        title: string,
-        videoId: string,
-        description: string,
-        teacher: {
-            name: string,
-            bio: string,
-            avatarURL: string
-        }
-    }
-}
 
 interface VideoProps {
     lessonSlug: string;
 }
 
 export function Video(props: VideoProps) {
-    const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+    const { data } = useGetLessonBySlugQuery({
         variables: {
             slug: props.lessonSlug,
         }
     });
 
-    if (!data) {
+    if (!data || !data.lesson) {
         return (
             <div className="flex-1">
                 <p>Carregando...</p>
@@ -72,26 +45,28 @@ export function Video(props: VideoProps) {
                             {data.lesson.description}
                         </p>
 
-                        <div className="flex items-center gap-4 mt-6">
-                            <img
-                                className="h-16 w-16 rounded-full border-2 border-blue-500"
-                                src={data.lesson.teacher.avatarURL}
-                                alt=""
-                            />
-                            <div className="leading-relaxed">
-                                <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
-                                <span className="text-gray-200 text-sn block">{data.lesson.teacher.bio}</span>
+                        {data.lesson.teacher && (
+                            <div className="flex items-center gap-4 mt-6">
+                                <img
+                                    className="h-16 w-16 rounded-full border-2 border-blue-500"
+                                    src={data.lesson.teacher.avatarURL}
+                                    alt=""
+                                />
+                                <div className="leading-relaxed">
+                                    <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
+                                    <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
+                                </div>
                             </div>
-                        </div>
+                        )}
                     </div>
 
                     <div className="flex flex-col gap-4">
-                        <a href="" className="p-4 text-sn bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
+                        <a href="" className="p-4 text-sm bg-green-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-green-700 transition-colors">
                             <DiscordLogo size={24} />
                             Comunidade do Discord
                         </a>
 
-                        <a href="" className="p-4 text-sn border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors">
+                        <a href="" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center rounded font-bold uppercase gap-2 justify-center hover:bg-blue-500 hover:text-gray-900 transition-colors">
                             <Lightning size={24} />
                             Acesse o desafio
                         </a>
@@ -108,7 +83,7 @@ export function Video(props: VideoProps) {
                         </div>
                         <div className="py-6 leading-relaxed">
                             <strong className="text-2xl">Material complementar</strong>
-                            <p className="text-sn text-gray-200 mt-2">
+                            <p className="text-sm text-gray-200 mt-2">
                                 Acesse o material complementar para acelerar o seu desenvolvimento
                             </p>
                         </div>
@@ -126,7 +101,7 @@ export function Video(props: VideoProps) {
                         </div>
                         <div className="py-6 leading-relaxed">
                             <strong className="text-2xl">Wallpapers exclusivos</strong>
-                            <p className="text-sn text-gray-200 mt-2">
+                            <p className="text-sm text-gray-200 mt-2">
                                 Baixe wallpapers exclusivos do Ignite Lab e personalize a sua máquina
                             </p>
                         </div>
--- ignite-lab-reactjs/src/lib/apollo.ts
+++ ignite-lab-reactjs/src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4u3vnvu0k3t01t6bt21784s/master',
+    uri: import.meta.env.VIT_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VIT_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 });
